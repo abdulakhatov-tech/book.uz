e008796db5bac3f8bf05a1f1ece738cdfc851a34
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-09-04 15:32:25 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button";
+
+const App:React.FC = () => {
+  return <div>
+    <h1 className="text-3xl font-bold underline text-[crimson]">
+      Hello world!
+    </h1>
+    <Button variant='default'>Login</Button>
+    <Button variant='destructive'>Login</Button>
+    <Button variant='ghost'>Login</Button>
+    <Button variant='link'>Login</Button>
+    <Button variant='outline'>Login</Button>
+    <Button variant='secondary'>Login</Button>
+  </div>;
+};
+
+export default App;
