966654e3f011cb064ce2d1e2835ae2b537dbd88f
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-09-18 04:51:44 +0500

    feat: added statistics

--- src/components/views/home/index.tsx
+++ src/components/views/home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Intro from "./intro";
 import FeaturedCards from "./featured-cards";
+import Statistics from "./statistics";
 
 const Home: React.FC = () => {
 	return (
 		<>
 			<Intro />
 			<FeaturedCards />
+			<Statistics />
 		</>
 	);
 };
