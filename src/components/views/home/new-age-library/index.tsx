5c554fcaa2de08ffc7ecc6ffadc98dae53023038
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-09-27 14:43:06 +0500

    feat(home): added recently-published-books section

--- src/components/views/home/new-age-library/index.tsx
+++ src/components/views/home/new-age-library/index.tsx
@@ -12,38 +12,41 @@ import useNewAgeLibraryFeatures from "./features";
 import BookSkeleton from "@/components/common/product-card-skeleton";
 
 const NewAgeLibrary: React.FC = () => {
-  const { t } = useTranslation();
-  const { books, loading, booksCount, handleMoreBooks, handleLessBooks } =
-    useNewAgeLibraryFeatures();
+	const { t } = useTranslation();
+	const { books, loading, booksCount, handleMoreBooks, handleLessBooks } =
+		useNewAgeLibraryFeatures();
 
-  return (
-    <Section id='new-age-library' className='bg-[#F0F0F0] py-[24px] md:py-[22px] lg:py-[36px] xl:py-[40px]'>
-      <Container>
-        <Button className='bg-orange hover:bg-orange mb-8'>
-          {t("home.new_age_library.title")}
-        </Button>
+	return (
+		<Section
+			id="new-age-library"
+			className="bg-[#F0F0F0] py-[24px] md:py-[22px] lg:py-[36px] xl:py-[40px]"
+		>
+			<Container>
+				<Button className="bg-orange hover:bg-orange mb-8">
+					{t("home.new_age_library.title")}
+				</Button>
 
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:3 md:gap-4'>
-          {loading
-            ? Array.from({ length: 12 }).map((_, idx) => (
-                <BookSkeleton key={idx} />
-              ))
-            : books?.map((book: BookI) => (
-                <ProductCard key={book._id} {...book} />
-              ))}
-        </div>
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:3 md:gap-4">
+					{loading
+						? Array.from({ length: 12 }).map((_, idx) => (
+								<BookSkeleton key={idx} />
+							))
+						: books?.map((book: BookI) => (
+								<ProductCard key={book._id} {...book} />
+							))}
+				</div>
 
-        {books.length > 11 && (
-          <div className='flex justify-center mt-8 gap-4'>
-            <MoreBooks onClick={handleMoreBooks} />
-            {booksCount > 11 && (
-              <MoreBooks title='Less books' onClick={handleLessBooks} />
-            )}
-          </div>
-        )}
-      </Container>
-    </Section>
-  );
+				{books.length > 11 && (
+					<div className="flex justify-center mt-8 gap-4">
+						<MoreBooks onClick={handleMoreBooks} />
+						{booksCount > 11 && (
+							<MoreBooks title="Less books" onClick={handleLessBooks} />
+						)}
+					</div>
+				)}
+			</Container>
+		</Section>
+	);
 };
 
 export default NewAgeLibrary;
--- src/services/categories/index.ts
+++ src/services/categories/index.ts
@@ -3,50 +3,50 @@ import { toast } from "@/components/ui/use-toast";
 import useQueryHandler from "@/hooks/useQueryHandler";
 
 const useCategoriesService = () => {
-  const axios = useAxiosInstance();
+	const axios = useAxiosInstance();
 
-  const handleError = (error: any, action: string) => {
-    toast({
-      title: `Error ${action}`,
-      description: error.message,
-    });
-  };
+	const handleError = (error: any, action: string) => {
+		toast({
+			title: `Error ${action}`,
+			description: error.message,
+		});
+	};
 
-  const getNewAgeLibrary = useQueryHandler({
-    queryKey: ["newAgeLibrary"],
-    queryFn: async () => {
-      const response = await axios.get("/category/new-age-library");
-      return response.data?.data || [];
-    },
-    refetchInterval: 60 * 60 * 1000, // 1 hour
-    onError: (error) => handleError(error, "fetching new age library"),
-  });
+	const getNewAgeLibrary = useQueryHandler({
+		queryKey: ["newAgeLibrary"],
+		queryFn: async () => {
+			const response = await axios.get("/category/new-age-library");
+			return response.data?.data || [];
+		},
+		refetchInterval: 60 * 60 * 1000, // 1 hour
+		onError: (error) => handleError(error, "fetching new age library"),
+	});
 
-  const getRecentlyPublishedBooks = useQueryHandler({
-    queryKey: ["recentlyPublishedBooks"],
-    queryFn: async () => {
-      const response = await axios.get("/category/recently-published");
-      return response.data?.data || [];
-    },
-    refetchInterval: 60 * 60 * 1000, // 1 hour
-    onError: (error) => handleError(error, "fetching recently published books"),
-  });
+	const getRecentlyPublishedBooks = useQueryHandler({
+		queryKey: ["recentlyPublishedBooks"],
+		queryFn: async () => {
+			const response = await axios.get("/category/recently-published");
+			return response.data?.data || [];
+		},
+		refetchInterval: 60 * 60 * 1000, // 1 hour
+		onError: (error) => handleError(error, "fetching recently published books"),
+	});
 
-  const getNewlyArrivedBooks = useQueryHandler({
-    queryKey: ["newlyArrivedBooks"],
-    queryFn: async () => {
-      const response = await axios.get("/category/newly-arrived");
-      return response.data?.data || [];
-    },
-    refetchInterval: 60 * 60 * 1000, // 1 hour
-    onError: (error) => handleError(error, "fetching newly arrived books"),
-  });
+	const getNewlyArrivedBooks = useQueryHandler({
+		queryKey: ["newlyArrivedBooks"],
+		queryFn: async () => {
+			const response = await axios.get("/category/newly-arrived");
+			return response.data?.data || [];
+		},
+		refetchInterval: 60 * 60 * 1000, // 1 hour
+		onError: (error) => handleError(error, "fetching newly arrived books"),
+	});
 
-  return {
-    getNewAgeLibrary,
-    getNewlyArrivedBooks,
-    getRecentlyPublishedBooks,
-  };
+	return {
+		getNewAgeLibrary,
+		getNewlyArrivedBooks,
+		getRecentlyPublishedBooks,
+	};
 };
 
 export default useCategoriesService;
