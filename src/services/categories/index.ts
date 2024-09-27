24d71498cee5a24cb940f6024439f9a4c960ec1e
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-09-27 11:06:45 +0500

    feat(home): added new-age-library

--- src/services/categories/index.ts
+++ src/services/categories/index.ts
@@ -3,28 +3,28 @@ import { toast } from "@/components/ui/use-toast";
 import useQueryHandler from "@/hooks/useQueryHandler";
 
 const useCategoriesService = () => {
-    const axios = useAxiosInstance();
+	const axios = useAxiosInstance();
 
-    const handleError = (error: any, action: string) => {
+	const handleError = (error: any, action: string) => {
 		toast({
 			title: `Error ${action}`,
 			description: error.message,
 		});
 	};
 
-    const getNewAgeLibrary = useQueryHandler({
-        queryKey: ['newAgeLibrary'],
-        queryFn: async () => {
-            const response = await axios.get('/category/new-age-library');
-            return response.data?.data || [];
-        },
-        refetchInterval: 60 * 60 * 1000, // 1 hour
-        onError: (error) => handleError(error, "fetching new age library"),
-    });
+	const getNewAgeLibrary = useQueryHandler({
+		queryKey: ["newAgeLibrary"],
+		queryFn: async () => {
+			const response = await axios.get("/category/new-age-library");
+			return response.data?.data || [];
+		},
+		refetchInterval: 60 * 60 * 1000, // 1 hour
+		onError: (error) => handleError(error, "fetching new age library"),
+	});
 
-  return {
-    getNewAgeLibrary
-  };
+	return {
+		getNewAgeLibrary,
+	};
 };
 
 export default useCategoriesService;
