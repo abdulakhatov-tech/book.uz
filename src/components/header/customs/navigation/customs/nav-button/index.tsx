5344d62e9afe47d1b8d67adda3701f13cb124d76
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-10-06 08:11:22 +0500

    fix: fixed product-card and other cards hover effect

--- src/components/header/customs/navigation/customs/nav-button/index.tsx
+++ src/components/header/customs/navigation/customs/nav-button/index.tsx
@@ -22,7 +22,9 @@ const NavButton: FC<NavButtonPropsI> = ({ Icon, label, path, count }) => (
 				<span className="border-white border-[2px] absolute -top-3 -right-3 px-3 max-w-9 h-[26px] rounded-full bg-orange text-white text-[14px] flex items-center justify-center">
 					{count < 10 ? count : `9+`}
 				</span>
-			) : ''}
+			) : (
+				""
+			)}
 		</NavLink>
 	</Button>
 );
