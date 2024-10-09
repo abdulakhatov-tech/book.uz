813ea9bad5a43bd1e589e529c36ec7c54db37ec6
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-10-10 03:53:46 +0500

    fix: fixed vulnaribilities

--- src/redux/slices/checkout/index.ts
+++ src/redux/slices/checkout/index.ts
@@ -1,5 +1,5 @@
 import { UserCheckoutInfo } from "@/types";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface InitialStateI {
 	userInfo: UserCheckoutInfo;
