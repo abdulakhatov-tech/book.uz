b50c24de5801d40a4f31a54d0d264641e03d7c94
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-10-11 23:41:30 +0500

    feat: added SEO

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 import { Provider } from "react-redux";
 import AuthProvider from "react-auth-kit";
 import { BrowserRouter } from "react-router-dom";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "react-photo-view/dist/react-photo-view.css";
